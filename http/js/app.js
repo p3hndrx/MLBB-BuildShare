fe914b3e95cc5abe668e3035ab96d6cd64585473
p3hndrx <14354635+p3hndrx@users.noreply.github.com>
2022-02-10 14:10:52 -0500

    Create Blank Template

--- http/js/app.js
+++ http/js/app.js
@@ -1,19 +1,23 @@
-var gear =document.querySelectorAll('.item');
-for(var i=0;i<gear.length;i++){
-gear[i].addEventListener('mouseover',hover,false);
-gear[i].addEventListener('mouseout',off,false);
-gear[i].addEventListener('click',selected,false);
+var gear = document.querySelectorAll('.item');
+for (var i = 0; i < gear.length; i++) {
+  gear[i].addEventListener('mouseover', hover, false);
+  gear[i].addEventListener('mouseout', off, false);
+  gear[i].addEventListener('click', selected, false);
 }
 
-function hover(){
-    this.classList.add("hover");
+function hover() {
+  this.classList.add("hover");
 }
-function off(){
-    this.classList.remove("hover");
+
+function off() {
+  this.classList.remove("hover");
 }
-function selected(){
-   if (this.classList.contains("active")==false){
-   		this.classList.add("active");
-				}else{
-   		this.classList.remove("active");}
+
+function selected() {
+  if (this.classList.contains("active") == false) {
+    this.classList.add("active");
+  } else {
+    this.classList.remove("active");
+  }
+
 }
