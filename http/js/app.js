7977d8ab0f5ea23c7cbee9fdcb52db742ad3e6cd
p3hndrx <14354635+p3hndrx@users.noreply.github.com>
2022-02-11 18:34:06 -0500

    Updated wireframe. Static elements

--- http/js/app.js
+++ http/js/app.js
@@ -1,8 +1,49 @@
+
+/*///BUILD LISTS */
+ $(document).ready(function() {
+    //BUILD HERO CATEGORIES
+    roles = ["Mage", "Tank", "Marksman", "Assassin", "Fighter", "Support" ];
+    roles.forEach(buildtabs);
+    function buildtabs(item, i) {
+        role = item.toLowerCase()
+        $("#hero-tabs-list").append("<li><a href=\"#hero-tab-"+role+"\">"+item+"</a></li>");
+        $("#hero-tabs").append("<div id=\"hero-tab-"+role+"\"><div class=\"hero-tab\" id=\"hero-selectable-"+role+"\"></div></div>");
+    }
+
+    //BUilD HERO BUTTONS
+    $.getJSON('../db/herodb.json', function(h) {
+        $.each(h.data, function(i,x) {
+            $("#hero-selectable").append("<div class=\"hero\" id=\""+x.id+"\">" + x.hero_name + "</div>");
+            role = x.class.toLowerCase()
+            $("#hero-selectable-"+role).append("<div class=\"hero\" id=\""+x.id+"\">" + x.hero_name + "</div>");
+            });
+        });
+
+    //BUILD ITEM CATEGORIES
+    itemcat = ["Defense", "Attack", "Magic", "Movement", "Roaming", "Jungling" ];
+    itemcat.forEach(cattabs);
+    function cattabs(item, i) {
+        equip = item.toLowerCase()
+        $("#cat-tabs-list").append("<li><a href=\"#tab-"+equip+"\">"+item+"</a></li>");
+        $("#item-tabs").append("<div id=\"tab-"+equip+"\"><div class=\"item-tab\" id=\"item-selectable-"+equip+"\"></div></div>");
+    }
+
+    //BUilD ITEM BUTTONS
+    $.getJSON('../db/itemdb.json', function(e) {
+        $.each(e.data, function(i,x) {
+                    $("#item-selectable").append("<div class=\"item\" id=\""+x.id+"\">" + x.item_name + "</div>");
+                    equip = x.item_category.toLowerCase()
+                    $("#item-selectable-"+equip).append("<div class=\"item\" id=\""+x.id+"\">" + x.item_name + "</div>");
+                });
+             });
+ });
+
+
 /* COLLAPSIBLE HEROES */
 $(function() {
   $("#hero-menu").tabs({
-    collapsible: true
-    /*active: false*/
+    collapsible: true,
+    active: false
   });
 });
 
@@ -17,7 +58,7 @@ $(function() {
     stop: function() {
       var result = $("#hero-result").empty();
       $(".ui-selected", this).each(function() {
-        var index = $("#hero-selectable div").index(this);
+        var index = $(".hero-tab div").index(this);
         result.append(this.id);
       });
     }
@@ -26,12 +67,12 @@ $(function() {
 
 /* TABBED ITEM */
 $(function() {
-  $("#tabs").tabs();
+  $("#item-tabs").tabs();
 });
 
 /* SELECTABLE ITEM */
 $(function() {
-  $(".menu").selectable({
+  $(".item-tab").selectable({
     stop: function() {
       var result = $("#select-result").empty();
       $(".ui-selected", this).each(function() {
@@ -47,3 +88,7 @@ $(function() {
   $("#sortable").sortable();
   $("#sortable").disableSelection();
 });
+
+
+
+
