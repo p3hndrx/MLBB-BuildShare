f8a7113c9a50c6a37a40de997e0ab9e69fe9d20a
p3hndrx <14354635+p3hndrx@users.noreply.github.com>
2022-02-11 15:15:58 -0500

    Updated wireframe

--- http/js/app.js
+++ http/js/app.js
@@ -1,43 +1,48 @@
+/* COLLAPSIBLE HEROES */
  $(function() {
    $("#hero-menu").tabs({
-     collapsible: true,
-     active: false
+     collapsible: true
+     /*active: false*/
    });
  });
 
+/* TABBED HEROES */
  $(function() {
    $("#hero-tabs").tabs();
  });
 
+/* SELECTABLE HEROES */
  $(function() {
    $(".hero-tab").selectable({
      stop: function() {
-       var result = $("#select-result").empty();
+       var result = $("#hero-result").empty();
        $(".ui-selected", this).each(function() {
-         var index = $("#selectable div").index(this);
-         result.append((index + 1));
+         var index = $("#hero-selectable div").index(this);
+         result.append(this.id);
        });
      }
    });
  });
 
-
+/* TABBED ITEM */
  $(function() {
    $("#tabs").tabs();
  });
 
+/* SELECTABLE ITEM */
  $(function() {
    $(".menu").selectable({
      stop: function() {
        var result = $("#select-result").empty();
        $(".ui-selected", this).each(function() {
          var index = $("#selectable div").index(this);
-         result.append((index + 1));
+         result.append(this.id);
        });
      }
    });
  });
 
+/* SORTABLE BUILD ITEM */
  $(function() {
    $("#sortable").sortable();
    $("#sortable").disableSelection();
