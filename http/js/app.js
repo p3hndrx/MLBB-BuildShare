61694f3a91acfa2038899b08f79c0c70cb070aa2
p3hndrx <14354635+p3hndrx@users.noreply.github.com>
2022-02-11 15:17:26 -0500

    Updated wireframe. Static elements

--- http/js/app.js
+++ http/js/app.js
@@ -1,49 +1,49 @@
 /* COLLAPSIBLE HEROES */
- $(function() {
-   $("#hero-menu").tabs({
-     collapsible: true
-     /*active: false*/
-   });
- });
+$(function() {
+  $("#hero-menu").tabs({
+    collapsible: true
+    /*active: false*/
+  });
+});
 
 /* TABBED HEROES */
- $(function() {
-   $("#hero-tabs").tabs();
- });
+$(function() {
+  $("#hero-tabs").tabs();
+});
 
 /* SELECTABLE HEROES */
- $(function() {
-   $(".hero-tab").selectable({
-     stop: function() {
-       var result = $("#hero-result").empty();
-       $(".ui-selected", this).each(function() {
-         var index = $("#hero-selectable div").index(this);
-         result.append(this.id);
-       });
-     }
-   });
- });
+$(function() {
+  $(".hero-tab").selectable({
+    stop: function() {
+      var result = $("#hero-result").empty();
+      $(".ui-selected", this).each(function() {
+        var index = $("#hero-selectable div").index(this);
+        result.append(this.id);
+      });
+    }
+  });
+});
 
 /* TABBED ITEM */
- $(function() {
-   $("#tabs").tabs();
- });
+$(function() {
+  $("#tabs").tabs();
+});
 
 /* SELECTABLE ITEM */
- $(function() {
-   $(".menu").selectable({
-     stop: function() {
-       var result = $("#select-result").empty();
-       $(".ui-selected", this).each(function() {
-         var index = $("#selectable div").index(this);
-         result.append(this.id);
-       });
-     }
-   });
- });
+$(function() {
+  $(".menu").selectable({
+    stop: function() {
+      var result = $("#select-result").empty();
+      $(".ui-selected", this).each(function() {
+        var index = $("#selectable div").index(this);
+        result.append(this.id);
+      });
+    }
+  });
+});
 
 /* SORTABLE BUILD ITEM */
- $(function() {
-   $("#sortable").sortable();
-   $("#sortable").disableSelection();
- });
+$(function() {
+  $("#sortable").sortable();
+  $("#sortable").disableSelection();
+});
