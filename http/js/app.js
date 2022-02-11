f4ed464ad0ef903cbfc9df632277304862e4d721
p3hndrx <14354635+p3hndrx@users.noreply.github.com>
2022-02-11 13:08:31 -0500

    Updated wireframe

--- http/js/app.js
+++ http/js/app.js
@@ -1,27 +1,44 @@
- $( function() {
-    $( "#hero-menu" ).tabs({
-      collapsible: true,
-      active: false
-    });
-  } );
+ $(function() {
+   $("#hero-menu").tabs({
+     collapsible: true,
+     active: false
+   });
+ });
 
-$( function() {
-    $( "#tabs" ).tabs();
-  } );
+ $(function() {
+   $("#hero-tabs").tabs();
+ });
 
-$( function() {
-    $( ".menu" ).selectable({
-      stop: function() {
-        var result = $( "#select-result" ).empty();
-        $( ".ui-selected", this ).each(function() {
-          var index = $( "#selectable div" ).index( this );
-          result.append( ( index + 1 ) );
-        });
-      }
-    });
-  } );
-  
- $( function() {
-    $( "#sortable" ).sortable();
-    $( "#sortable" ).disableSelection();
-  } );
+ $(function() {
+   $(".hero-tab").selectable({
+     stop: function() {
+       var result = $("#select-result").empty();
+       $(".ui-selected", this).each(function() {
+         var index = $("#selectable div").index(this);
+         result.append((index + 1));
+       });
+     }
+   });
+ });
+
+
+ $(function() {
+   $("#tabs").tabs();
+ });
+
+ $(function() {
+   $(".menu").selectable({
+     stop: function() {
+       var result = $("#select-result").empty();
+       $(".ui-selected", this).each(function() {
+         var index = $("#selectable div").index(this);
+         result.append((index + 1));
+       });
+     }
+   });
+ });
+
+ $(function() {
+   $("#sortable").sortable();
+   $("#sortable").disableSelection();
+ });
