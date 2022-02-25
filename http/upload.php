86b8826160df4be468379c58a68f431bc52f3236
p3hndrx <14354635+p3hndrx@users.noreply.github.com>
2022-02-25 17:48:26 -0500

    Test Logging

--- http/upload.php
+++ http/upload.php
@@ -8,6 +8,7 @@ define('UPLOAD_DIR', 'export/');
 $img = $_POST['imgBase64'];
 $name = $_POST['name'];
 $reference = $_POST['ref'];
+
 $img = str_replace('data:image/png;base64,', '', $img);
 $img = str_replace(' ', '+', $img);
 $data = base64_decode($img);
@@ -19,7 +20,7 @@ print $success ? $file : 'Unable to save the file.';
 $log = UPLOAD_DIR  . 'bs.log';
 if (strpos(file_get_contents($log), $name) == false)
 {
-        $txt = $name . "," . $reference
+        $txt = $name . ", " . $reference;
         $myfile = file_put_contents($log, $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
  }
 
