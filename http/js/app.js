5e55f83912efd805317779038c38581c86c45758
p3hndrx <14354635+p3hndrx@users.noreply.github.com>
2022-03-11 02:20:19 -0500

    Added Blessing Slot

--- http/js/app.js
+++ http/js/app.js
@@ -18,11 +18,12 @@ var opt_slot = {
   0: [],
   1: []
 };
-var rj_slot = 0;
-//var herojson = '../../MLBB-API/v1/hero-meta-final.json'
-//var itemjson = '../../MLBB-API/v1/item-meta-final.json'
-var herojson = 'db/hero-meta-final.json'
-var itemjson = 'db/item-meta-final.json'
+var blessingData = []
+var bless_slot = 0
+var herojson = '../../MLBB-API/v1/hero-meta-final.json'
+var itemjson = '../../MLBB-API/v1/item-meta-final.json'
+//var herojson = 'db/hero-meta-final.json'
+//var itemjson = 'db/item-meta-final.json'
 
 /*///BUILD LISTS */
 $(document).ready(function() {
@@ -56,10 +57,10 @@ $(document).ready(function() {
     equip = item.toLowerCase()
     $("#cat-tabs-list").append("<li><a href=\"#tab-" + equip + "\">" + item + "</a></li>");
     if (equip != "roaming" && equip != "jungling") {
-        $("#item-tabs").append("<div id=\"tab-" + equip + "\"><div class=\"item-tab\" id=\"item-selectable-" + equip + "\"></div></div>");
-        } else {
-        $("#item-tabs").append("<div id=\"tab-" + equip + "\"><div class=\"bless-tab\" id=\"bless-selectable-" + equip + "\"></div></div>");
-        }
+      $("#item-tabs").append("<div id=\"tab-" + equip + "\"><div class=\"item-tab\" id=\"item-selectable-" + equip + "\"></div></div>");
+    } else {
+      $("#item-tabs").append("<div id=\"tab-" + equip + "\"><div class=\"bless-tab\" id=\"bless-selectable-" + equip + "\"></div></div>");
+    }
   }
 
   //BUilD ITEM BUTTONS
@@ -71,10 +72,10 @@ $(document).ready(function() {
       if (equip != "roaming" && equip != "jungling") {
         $("#item-selectable").append("<div class=\"item\" id=\"" + x.id + "\" style=\"background-image: url(./img/items/" + x.icon + ")\"><span class=\"itemtip\">" + x.item_name + "</span></div>");
         $("#item-selectable-" + equip).append("<div class=\"item\" id=\"" + x.id + "\" style=\"background-image: url(./img/items/" + x.icon + ")\"><span class=\"itemtip\">" + x.item_name + "</span></div>");
-        } else {
+      } else {
         $("#bless-selectable").append("<div class=\"bless\" id=\"" + x.id + "\" style=\"background-image: url(./img/items/" + x.icon + ")\"><span class=\"itemtip\">" + x.item_name + "</span></div>");
         $("#bless-selectable-" + equip).append("<div class=\"bless\" id=\"" + x.id + "\" style=\"background-image: url(./img/items/" + x.icon + ")\"><span class=\"itemtip\">" + x.item_name + "</span></div>");
-        }
+      }
     });
   });
 });
@@ -316,7 +317,7 @@ function getItemData(code) {
 
             for (var key in skill) {
               value = skill[key];
-              console.log(value);
+              //console.log(value);
               if (value !== "null" && key !== "modifiers") {
                 upassiveexist = 1;
               } else {
@@ -401,25 +402,53 @@ function getBlessData(code) {
   $.getJSON(itemjson, function(h) {
     $.each(h.data, function(i, x) {
       if (x.id == code) {
-        /*console.log(x.id);
-        console.log(x.icon);
-        console.log(x.item_name);
-        console.log(x.item_category);*/
+
         blessingData = []
         icon = "url(./img/items/" + x.icon + ");"
         blessingData = [x.id, x.icon, x.item_name, x.item_category];
-        console.log("Blessing Items:" + blessingData);
+        //console.log("Blessing Items:" + blessingData);
 
         /*UPDATE ICON*/
         blesspic = document.getElementById("bless-slot");
-        $(blesspic).attr("style", "background-image:" + icon);
+        blesspic.innerHTML = "";
 
-        /*BUILD EXPORTABLE
-        expport = document.getElementById("exp-port");
-        $(expport).attr("style", "background-image:" + icon);
-        expherolabel = document.getElementById("exp-hero-name");
-        expherolabel.innerHTML = "";
-        expherolabel.append(x.hero_name);*/
+        exp_slot = document.getElementById("exp-bless-slot");
+        exp_slot.style.visibility = "visible";
+        exp_slot.innerHTML = "";
+
+        /* BUILD CLOSE BUTTON*/
+        var slot_div = document.createElement('div');
+        slot_div.classList.add('box_select');
+        icon = "url(./img/items/" + blessingData[1] + ");"
+        $(slot_div).attr("style", "background-image:" + icon);
+        var slot_tip = document.createElement('span');
+        $(slot_tip).attr("class", "optslottip");
+        slot_tip.innerHTML = blessingData[2];
+        slot_div.appendChild(slot_tip);
+        var slot_close = document.createElement('a');
+        slot_close.setAttribute('href', 'javascript:;');
+        slot_close.addEventListener("click", blessing_deselect);
+        slot_close.style.fontSize = '20px';
+        var closeNode = document.createElement('div');
+        closeNode.setAttribute('class', 'closebutt');
+        slot_close.appendChild(closeNode)
+        slot_div.appendChild(slot_close);
+
+
+        /* FORMAT EXPORT */
+        var exp_slot_div = document.createElement('div');
+        exp_slot_div.classList.add('exp_box_select');
+        $(exp_slot_div).attr("style", "background-image:" + icon);
+        var exp_slot_tip = document.createElement('span');
+        $(exp_slot_tip).attr("class", "exp_optslottip");
+        exp_slot_tip.innerHTML = blessingData[2];
+        exp_slot.appendChild(exp_slot_tip);
+        exp_slot.appendChild(exp_slot_div);
+        /* END FORMAT EXPORT */
+
+        blesspic.appendChild(slot_div);
+
+        /* END FORMAT EXPORT */
 
         /*BLESSING INFO*/
         var itemoverview = document.getElementById("select-result")
@@ -604,7 +633,7 @@ function getBlessData(code) {
 
             for (var key in skill) {
               value = skill[key];
-              console.log(value);
+              //console.log(value);
               if (value !== "null" && key !== "modifiers") {
                 upassiveexist = 1;
               } else {
@@ -676,9 +705,6 @@ function getBlessData(code) {
           attribox.append(upassivebox);
         });
 
-
-
-
         return selected_items;
       }
     });
@@ -702,7 +728,7 @@ $(function() {
           });
         });
         hero_choice = this.id;
-        global(hero_choice, item_slot, opt_slot);
+        global(hero_choice, item_slot, opt_slot, bless_slot);
       });
     }
   });
@@ -715,114 +741,31 @@ $(function() {
       var result = $("#bless-slot").empty();
       $(".ui-selected", this).each(function() {
         var index = $(".bless-tab div").index(this);
+        blessingData = []
         getBlessData(this.id);
         bless_slot = this.id;
         console.log(bless_slot)
-        global(hero_choice, item_slot, opt_slot);
+        global(hero_choice, item_slot, opt_slot, bless_slot);
       });
     }
   });
 });
 
+/* BLESSING DESELECT */
+function blessing_deselect() {
+  $('.bless').removeClass('ui-selected')
+  console.log("Clearing Blessing")
+  blesspic = document.getElementById("bless-slot");
+  blesspic.innerHTML = "";
 
-/* TABBED ITEM */
-$(function() {
-  $("#item-tabs").tabs();
-});
+  exp_slot = document.getElementById("exp-bless-slot");
+  exp_slot.innerHTML = "";
+  exp_slot.style.visibility = "hidden";
 
-/* TABBED BUILDER */
-$(function() {
-  $("#dropslots").tabs();
-});
-
-/* GLOBAL MANIPS */
-function global(hero_choice, item, optional) {
-  buildstring = JSON.stringify(item);
-
-  //display feedback:
-  build = buildstring + "-" + JSON.stringify(optional);
-  build = build.replace(/\[\]/g, "");
-  build = build.replace(/[,]/g, "");
-  build = build.replace(/["']/g, "");
-  build = build.replace(/[0-9]:/g, "");
-  build = build.replace(/[\[\]]/g, "");
-  build = build.replace(/[{}]/g, "");
-
-  //construct full build
-  full_build = hero_choice + "-" + build
-  full_build_enc = btoa(full_build);
-  full_build_hash = md5(full_build_enc);
-
-  console.log(full_build)
-  console.log(full_build_enc)
-  var buildbox = document.getElementById("build-feedback");
-
-  if (hero_choice == 0 || buildstring.includes("[]") == true) {
-    //buildbox.innerHTML = full_build + "<br>" + full_build_enc
-    buildbox.innerHTML = "This build requires more info..."
-    //document.getElementById("build-copy").innerHTML = '';
-    if (hero_choice == 0) {
-      buildbox.innerHTML += "<li>Please select a hero."
-    }
-    if (buildstring.includes("[]") == true) {
-      buildbox.innerHTML += "<li>Please slot more items."
-    }
-  } else {
-    $("#dropslots").tabs("option", "active", 1);
-    buildbox.innerHTML = "<form><textarea rows='4' cols='50' id='build-code' class='buildtext' onclick='copytoclip();'>" + full_build_hash + "</textarea>"
-    buildbox.innerHTML += "<br>You can use this code with the Discord Bot! <br>OR...<div class='sharebox'><a href='javascript:download();' class='sharebutt'>Download</a></div>"
-
-  }
+  blessingData = []
+  bless_slot = 0
+  global(hero_choice, item_slot, opt_slot, bless_slot);
 };
-
-/*COPY TO CLIPBOARD*/
-function copytoclip() {
-  var copyText = document.getElementById("build-code");
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); /* For mobile devices */
-  navigator.clipboard.writeText(copyText.value);
-  document.getElementById("build-copy").innerHTML = "<br>Copied to Clipboard.";
-  upload();
-} /*END COPY-TO-CLIP
-
-/*UPLOAD*/
-function upload() {
-    //console.log("Posting: " + full_build_enc);
-    html2canvas(document.querySelector("#export"), {
-      onclone: function(clonedDoc) {
-        clonedDoc.getElementById('export').style.display = 'block';
-      }
-    }).then(canvas => {
-
-      var dataNAME = full_build_hash
-      var dataREF = full_build_enc
-      var dataURL = canvas.toDataURL("image/png");
-
-      $.ajax({
-        type: "POST",
-        url: "upload.php",
-        data: {
-          name: dataNAME,
-          imgBase64: dataURL,
-          ref: dataREF
-        }
-      }).done(function(o) {
-        console.log('saved:'+dataREF);
-      });
-    });
-  };
-
-/*HTML2 CANVAS */
-function download() {
-  html2canvas(document.querySelector("#export"), {
-    onclone: function(clonedDoc) {
-      clonedDoc.getElementById('export').style.display = 'block';
-    }
-  }).then(canvas => {
-    //document.body.appendChild(canvas)
-    saveAs(canvas.toDataURL(), full_build_hash + '.png');
-  });
-}
 
 /* LOADABLE ITEMS */
 $(function() {
@@ -903,7 +846,7 @@ $(function() {
         selected_items = [];
       }
 
-      global(hero_choice, item_slot, opt_slot);
+      global(hero_choice, item_slot, opt_slot, bless_slot);
       console.log(item_slot);
       console.log(opt_slot);
 
@@ -946,7 +889,6 @@ $(function() {
         slot_div.classList.add('box_select');
         icon = "url(./img/items/" + selected_items[1] + ");"
         $(slot_div).attr("style", "background-image:" + icon);
-        $(slot_div).attr("style", "background-image:" + icon);
         var slot_tip = document.createElement('span');
         $(slot_tip).attr("class", "optslottip");
         slot_tip.innerHTML = selected_items[2];
@@ -959,13 +901,6 @@ $(function() {
         closeNode.setAttribute('class', 'closebutt');
         slot_close.appendChild(closeNode)
         slot_div.appendChild(slot_close);
-
-        var small_node = document.createElement('small');
-        small_node.style.fontSize = '20px';
-        //var txNode = document.createTextNode(selected_items[x]);
-        //small_node.appendChild(txNode);
-        //slot_div.appendChild(document.createElement('br'));
-        //slot_div.appendChild(small_node);
 
         /* FORMAT EXPORT */
         var exp_slot_div = document.createElement('div');
@@ -984,7 +919,7 @@ $(function() {
         selected_items = [];
       }
 
-      global(hero_choice, item_slot, opt_slot);
+      global(hero_choice, item_slot, opt_slot, bless_slot);
       console.log(item_slot);
       console.log(opt_slot);
     }
@@ -1001,6 +936,7 @@ $(function() {
         //selected_items = []
         //selected_items.push(this.id);
         getItemData(this.id);
+        highlight();
         //result.append(this.id);
       });
     },
@@ -1014,6 +950,7 @@ $(function() {
     eval(streval);
     $("#item-slot-" + i).click(function(e) { //  alert();
       e.preventDefault();
+      dim();
       ItemLoader.clickHandler(e)
     });
   }
@@ -1024,11 +961,145 @@ $(function() {
     eval(streval);
     $("#opt-slot-" + i).click(function(e) { //  alert();
       e.preventDefault();
+      dim();
       OptionLoader.clickHandler(e)
     });
   }
 
 }); //END LOADABLE
+
+// HIGHLIGHT BOX
+function highlight() {
+  for (let i = 0; i <= 5; i++) {
+    let divElement = document.querySelector("#item-slot-" + i);
+    divElement.classList.add("highlight");
+  }
+  for (let i = 0; i <= 1; i++) {
+    let div = document.querySelector("#opt-slot-" + i);
+    div.classList.add("highlight");
+  }
+}
+
+function dim() {
+  for (let i = 0; i <= 5; i++) {
+    let divElement = document.querySelector("#item-slot-" + i);
+    divElement.classList.remove("highlight");
+  }
+  for (let i = 0; i <= 1; i++) {
+    let div = document.querySelector("#opt-slot-" + i);
+    div.classList.remove("highlight");
+  }
+}
+
+
+/* BLESSING TAB SWITCH */
+$(function() {
+  $("#bless-slot").bind("click", function(event) {
+    //$("#item-tabs").tabs("option", "active", 6);
+  });
+});
+
+
+
+/* TABBED ITEM */
+$(function() {
+  $("#item-tabs").tabs();
+});
+
+/* TABBED BUILDER */
+$(function() {
+  $("#dropslots").tabs();
+});
+
+/* GLOBAL MANIPS */
+function global(hero_choice, item, optional, blessing) {
+  buildstring = JSON.stringify(item);
+
+  //display feedback:
+  build = buildstring + "-" + JSON.stringify(optional);
+  build = build.replace(/\[\]/g, "");
+  build = build.replace(/[,]/g, "");
+  build = build.replace(/["']/g, "");
+  build = build.replace(/[0-9]:/g, "");
+  build = build.replace(/[\[\]]/g, "");
+  build = build.replace(/[{}]/g, "");
+
+  //construct full build
+  full_build = hero_choice + "-" + build + "-" + blessing
+  full_build_enc = btoa(full_build);
+  full_build_hash = md5(full_build_enc);
+
+  console.log(full_build)
+  console.log(full_build_enc)
+  var buildbox = document.getElementById("build-feedback");
+
+  if (hero_choice == 0 || buildstring.includes("[]") == true) {
+    //buildbox.innerHTML = full_build + "<br>" + full_build_enc
+    buildbox.innerHTML = "This build requires more info..."
+    //document.getElementById("build-copy").innerHTML = '';
+    if (hero_choice == 0) {
+      buildbox.innerHTML += "<li>Please select a hero."
+    }
+    if (buildstring.includes("[]") == true) {
+      buildbox.innerHTML += "<li>Please slot more items."
+    }
+  } else {
+    $("#dropslots").tabs("option", "active", 1);
+    buildbox.innerHTML = "<form><textarea rows='4' cols='50' id='build-code' class='buildtext' onclick='copytoclip();'>" + full_build_hash + "</textarea>"
+    buildbox.innerHTML += "<br>You can use this code with the Discord Bot! <br>OR...<div class='sharebox'><a href='javascript:download();' class='sharebutt'>Download</a></div>"
+
+  }
+};
+
+/*COPY TO CLIPBOARD*/
+function copytoclip() {
+  var copyText = document.getElementById("build-code");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+  navigator.clipboard.writeText(copyText.value);
+  document.getElementById("build-copy").innerHTML = "<br>Copied to Clipboard.";
+  upload();
+}
+/*END COPY-TO-CLIP
+
+/*UPLOAD*/
+function upload() {
+  //console.log("Posting: " + full_build_enc);
+  html2canvas(document.querySelector("#export"), {
+    onclone: function(clonedDoc) {
+      clonedDoc.getElementById('export').style.display = 'block';
+    }
+  }).then(canvas => {
+
+    var dataNAME = full_build_hash
+    var dataREF = full_build_enc
+    var dataURL = canvas.toDataURL("image/png");
+
+    $.ajax({
+      type: "POST",
+      url: "upload.php",
+      data: {
+        name: dataNAME,
+        imgBase64: dataURL,
+        ref: dataREF
+      }
+    }).done(function(o) {
+      console.log('saved:' + dataREF);
+    });
+  });
+};
+
+/*HTML2 CANVAS */
+function download() {
+  html2canvas(document.querySelector("#export"), {
+    onclone: function(clonedDoc) {
+      clonedDoc.getElementById('export').style.display = 'block';
+    }
+  }).then(canvas => {
+    //document.body.appendChild(canvas)
+    saveAs(canvas.toDataURL(), full_build_hash + '.png');
+  });
+}
 
 /*SAVE AS*/
 function saveAs(uri, filename) {
@@ -1055,13 +1126,6 @@ function saveAs(uri, filename) {
 
   }
 }
-
-
-/* SORTABLE BUILD ITEM  */
-$(function() {
-  //$("#sortable").sortable();
-  //$("#sortable").disableSelection();
-});
 
 /*HELP DIALOG*/
 $(function() {
